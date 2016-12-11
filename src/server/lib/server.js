ea5bf3b299e07e16f52a145fc833e1d8e37b4152
Marcel Valdez <marcel.valdez@live.com>
2016-12-10 22:09:46 -0800

    Moved server/src to server/lib

--- src/server/lib/server.js
+++ src/server/lib/server.js
@@ -1,15 +1,15 @@
-var express = require('express');
+const express = require('express');
 
 module.exports = function () {
-  var app = express();
-  app.put('/increments', function (req, res) {
+  const app = express();
+  app.put('/increments', (req, res) => {
     res.status(200).send('ok');
     //do some shit!
     //use redis!
   });
 
-  var server = app.listen(3000, function() {
-    var port = server.address().port;
+  const server = app.listen(3000, () => {
+    const port = server.address().port;
     console.log('Express REST services listening on port %s', port);
   });
 
