6463ead53459e85ae37220bf9df42e0608a6288a
Marcel Valdez <marcel.valdez@live.com>
2017-01-29 19:01:17 -0800

    Added karma configuration for client test runs

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,17 +1,47 @@
+var path = require('path');
+var fs = require('fs');
+var clientTestFiles = getTestFiles('client/test/');
+var commonTestFiles = getTestFiles('common/test/');
+
+var INIT_FILE = ['client/test/init.js'];
+var files = INIT_FILE.concat(clientTestFiles.concat(commonTestFiles));
+
+function getTestFiles(dir) {
+  var topFiles = fs.readdirSync(dir).map(f => dir + f)
+      .filter(f => f.endsWith('test.js') || f.endsWith('test.jsx'));
+  var subdirs = fs.readdirSync(dir)
+      .filter(file => fs.lstatSync(dir + file).isDirectory());
+
+  return subdirs.map(subdir => getTestFiles(dir + subdir + '/'))
+    .reduce((files, subdirFiles) => files.concat(subdirFiles), topFiles);
+}
+
+
+function webpackConfig() {
+  var config = require('./client.webpack.config');
+  config.entry = undefined;
+  return config;
+}
+
+var jsPreprocessors = ['webpack', 'sourcemap'];
 module.exports = function(config) {
   config.set({
-    plugins: ['karma-chrome-launcher', 'karma-mocha'],
-    browsers: ['Chrome'],
-    frameworks: ['mocha'],
-    files: [
-      { pattern: 'client/test/*test.js' },
-      { pattern: 'client/test/**/*test.js' }
+    plugins: [
+      require('karma-chrome-launcher'),
+      require('karma-mocha'),
+      require('karma-chai'),
+      require('karma-webpack'),
+      require('karma-sourcemap-loader')
     ],
+    browsers: ['Chrome'],
+    frameworks: ['mocha', 'chai'],
+    files: files,
     preprocessors: {
-      'test/*test.js': ['webpack'],
-      'test/**/*test.js': ['webpack']
+      'common/**/*.js': jsPreprocessors,
+      'client/**/*.js': jsPreprocessors,
+      'client/**/*.jsx': jsPreprocessors
     },
-    webpack: {},
+    webpack: webpackConfig(),
     webpackMiddleware: {
       stats: 'errors-only'
     }
